a65bad50125319aefb7b08097ab426d7ccac18b3
Marcos <mjr7195@psu.edu>
2023-09-12 18:54:21 -0400

    need to do while loop

--- Main.java
+++ Main.java
@@ -47,7 +47,7 @@ public class Main {
        /* end candy category */
 
        /* cookies category */
-       Cookies ChocoChipCK = new Cookies("Chocolate Chip Cookie", "Chocolate chip", 3.00, 170, ItemCategories.COOKIES);
+       Cookies chocoChipCK = new Cookies("Chocolate Chip Cookie", "Chocolate chip", 3.00, 170, ItemCategories.COOKIES);
        Cookies sugarCK = new Cookies("Sugar Cookie", "Sugar", 3.00, 140, ItemCategories.COOKIES);
        Cookies oreoCK = new Cookies("Oreo Cookie", "chocolate with creme", 3.00, 160, ItemCategories.COOKIES);
        Cookies mapleWalnutCK = new Cookies("Maple Walnut Cookie", "maple walnut", 3.00, 140, ItemCategories.COOKIES);
@@ -55,9 +55,75 @@ public class Main {
        Cookies smoreCK = new Cookies("S'more Cookie", "Marshmallow Chocolate", 3.00, 190, ItemCategories.COOKIES);
        /*end cookie category*/
 
-       System.out.println("Item selected: " + cokeOriginal.getName());
-       System.out.println("Item price: $" + hersheyMilkChoco.calcTotalPrice());
-       System.out.println(cokeOriginal.getName()+ " is a "+ cokeOriginal.getItemCategory());
+       //start vending machine
+
+
+       VendingMachine vendingMachine = new VendingMachine(6, 6);
+
+       //add products to array
+       /*first row*/
+       vendingMachine.addProduct(0,0, cokeOriginal.getName());
+       vendingMachine.addProduct(0,1, cokeRaspberry.getName());
+       vendingMachine.addProduct(0,2, cokeCherry.getName());
+       vendingMachine.addProduct(0,3, spriteOriginal.getName());
+       vendingMachine.addProduct(0,4, spriteLymonade.getName());
+       vendingMachine.addProduct(0,5, spriteZero.getName());
+
+       /* second row */
+       vendingMachine.addProduct(1,0, laysOriginal.getName());
+       vendingMachine.addProduct(1,1, laysBBQ.getName());
+       vendingMachine.addProduct(1,2, laysChileLimon.getName());
+       vendingMachine.addProduct(1,3, doritosSpicySweetChili.getName());
+       vendingMachine.addProduct(1,4, doritosOriginal.getName());
+       vendingMachine.addProduct(1,5, doritosReducedFat.getName());
+
+       // Third Row
+       vendingMachine.addProduct(2,0, hersheyMilkChoco.getName());
+       vendingMachine.addProduct(2,1, hersheyAlmonds.getName());
+       vendingMachine.addProduct(2,2, hersheyCookiesNcream.getName());
+       vendingMachine.addProduct(2,3, hersheyNuggets.getName());
+       vendingMachine.addProduct(2,4, hersheyKisses.getName());
+       vendingMachine.addProduct(2,5, hersheyCottonCandy.getName());
+
+       // Forth row
+       vendingMachine.addProduct(3,0, cherryGarcia.getName());
+       vendingMachine.addProduct(3,1, chocoChipCookieDough.getName());
+       vendingMachine.addProduct(3,2, mintChocoCookie.getName());
+       vendingMachine.addProduct(3,3, pbCup.getName());
+       vendingMachine.addProduct(3,4, sbCheesecake.getName());
+       vendingMachine.addProduct(3,5, saltedCara.getName());
+
+       // Fifth row
+       vendingMachine.addProduct(4,0, skittles.getName());
+       vendingMachine.addProduct(4,1, candyCane.getName());
+       vendingMachine.addProduct(4,2, starburst.getName());
+       vendingMachine.addProduct(4,3, tootsiePop.getName());
+       vendingMachine.addProduct(4,4, twizzler.getName());
+       vendingMachine.addProduct(4,5, candyCorn.getName());
+
+       // Sixth row
+       vendingMachine.addProduct(5,0, chocoChipCK.getName());
+       vendingMachine.addProduct(5,1, sugarCK.getName());
+       vendingMachine.addProduct(5,2, oreoCK.getName());
+       vendingMachine.addProduct(5,3, mapleWalnutCK.getName());
+       vendingMachine.addProduct(5,4, cinnamonCk.getName());
+       vendingMachine.addProduct(5,5, smoreCK.getName());
+
+       Scanner scanner = new Scanner(System.in);
+       //welcome user
+       System.out.println("Welcome to my Vending Machine!");
+       System.out.println("Here are the available items:");
+       System.out.println(); //for readability
+
+       //display array
+       vendingMachine.display();
+       System.out.println();
+       System.out.println("Input Format: rows(0-5) - columns(0-5)");
+       System.out.println("Please choose your desired row from the table above: ");
+
+
+
+
 
     }
 }
---
+++ VendingMachine.java
@@ -0,0 +1,45 @@
+public class VendingMachine {
+   private String[][] products;
+
+   public VendingMachine(int rows, int cols){
+       products = new String[rows][cols];
+   }
+   public void addProduct(int rows, int cols, String productName){
+       if (rows >= 0 && rows < products.length && cols >= 0 && cols < products[0].length){
+           products[rows][cols] = productName;
+       } else {
+           System.out.println("Invalid input for adding product.");
+       }
+   }
+   public String getProduct(int rows, int cols){
+       if (rows >= 0 && rows < products.length && cols >= 0 && cols < products.length){
+           String product = products[rows][cols];
+           if (product != null){
+               products[rows][cols] = null;
+               return product;
+           } else{
+               return "Already empty";
+           }
+       }else {
+           return "Location doesn't exist";
+       }
+   }
+
+   public void display(){
+       for (int i = 0; i < products.length;i++){
+           for (int j = 0; j < products[0].length; j++){
+               String product = products[i][j];
+               if (product == null){
+                   System.out.print("Empty");
+               } else {
+                   System.out.print(" (" + i + "-" + j + ") [ "+ product + " ]");
+               }
+           }
+           System.out.println();
+       }
+   }
+
+
+
+
+}
